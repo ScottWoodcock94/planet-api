5348f09feb26e297ced616b057500c20b24d2c34
ScottWoodcock94 <scottwoodcock94.official@gmail.com>
2024-09-20 13:46:16 +0100

    Created mock database.

--- index.ts
+++ index.ts
@@ -1,37 +1,44 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
 import 'dotenv/config';
 
 const app = express();
 const port = process.env.PORT || 4000;
 
-const styles = "color: purple; line-height: 100vh; text-align: center; margin: 0; font-family: sans-serif;"
+const styles =
+  'color: purple; line-height: 100vh; text-align: center; margin: 0; font-family: sans-serif;';
 
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res, next) => {
-    res.send(`<body style="margin: 0;"><h1 style="${styles}">Welcome to Planet! 🪐</h1></body>`);
+  res.send(
+    `<body style="margin: 0;"><h1 style="${styles}">Welcome to Planet! 🪐</h1></body>`
+  );
 });
 
 app.post('/', (req, res, next) => {
-    console.log(`Request Body: \n\n`, req.body);
-    res.send(req.body);
+  console.log(`Request Body: \n\n`, req.body);
+  res.send(req.body);
 });
 
 app.patch('/', (req, res, next) => {
-    console.log(`Request Body: \n\n`, req.body);
-    res.send(req.body);
+  console.log(`Request Body: \n\n`, req.body);
+  res.send(req.body);
 });
 
 app.put('/', (req, res, next) => {
-    console.log(`Request Body: \n\n`, req.body);
-    res.send(req.body);
+  console.log(`Request Body: \n\n`, req.body);
+  res.send(req.body);
 });
 
 app.delete('/', (req, res, next) => {
-    console.log(`Request Body: \n\n`, req.body);
-    res.send(req.body);
+  console.log(`Request Body: \n\n`, req.body);
+  res.send(req.body);
 });
 
-app.listen(port, () => console.log(`Planet Server API running at [:: => http://localhost:${port} <=::]`));
+app.listen(port, () =>
+  console.log(
+    `Planet Server API running at [:: => http://localhost:${port} <=::]`
+  )
+);
